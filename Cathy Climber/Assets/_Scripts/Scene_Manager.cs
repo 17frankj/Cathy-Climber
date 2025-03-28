ebbe74217e30883a33249cdfbe2033d3db359d92
Joshua David Frank <17joshuafrank@gmail.com>
2025-03-28 15:49:18 -0400

    added scene transition

--- Cathy Climber/Assets/_Scripts/Scene_Manager.cs
+++ Cathy Climber/Assets/_Scripts/Scene_Manager.cs
@@ -1,18 +1,24 @@
 using System;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 
 public class Scene_Manager : MonoBehaviour
 {
     [SerializeField] GameObject player;
     [SerializeField] String mainScene;
+    [SerializeField] String tutorial;
 
     void OnCollisionEnter(Collision collision)
     {
         LoadMainLevel();
     }
 
+    public void Loadtutorial()
+    {
+        SceneManager.LoadSceneAsync(tutorial);
+    }
     private void LoadMainLevel()
     {
         SceneManager.LoadSceneAsync(mainScene);
