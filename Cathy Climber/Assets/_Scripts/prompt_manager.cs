be118c3a2a6dd2f8a8d94f3a54cea84827c617be
Joshua David Frank <17joshuafrank@gmail.com>
2025-03-27 21:25:17 -0400

    added new tutorial messages

--- Cathy Climber/Assets/_Scripts/prompt_manager.cs
+++ Cathy Climber/Assets/_Scripts/prompt_manager.cs
@@ -6,6 +6,8 @@ public class prompt_manager : MonoBehaviour
 {
     [SerializeField] GameObject player;
     [SerializeField] GameObject Canvas;
+    public static int tracker = 0;
+   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,14 +22,48 @@ public class prompt_manager : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        FirstMessage();
+        switch(tracker)
+        {
+            case 0: 
+                TurnOffFirstMessage();
+                return;
+            case 1:
+                DisplaySecondMessage();
+                return;
+            case 2:
+                TurnOffSecondMessage();
+                return;
+            default:
+                return;
+        }
     }
 
-    public void FirstMessage()
+    public void TurnOffSecondMessage()
+    {
+        if(player.CompareTag("Player"))
+        {
+            Canvas.transform.GetChild(1).gameObject.SetActive(false); // turn off on second message
+            tracker++;
+            gameObject.SetActive(false); // turn off trigger;
+        }
+    }
+    public void DisplaySecondMessage()
+    {
+        if(player.CompareTag("Player"))
+        {
+            Canvas.transform.GetChild(1).gameObject.SetActive(true); // turn on second message
+            tracker++;
+            gameObject.SetActive(false); // turn off trigger;
+        }
+    }
+
+    public void TurnOffFirstMessage()
     {
         if(player.CompareTag("Player"))
         {
             Canvas.transform.GetChild(0).gameObject.SetActive(false); // turn off first tutorial message
+            tracker++;
+            gameObject.SetActive(false); // turn off trigger;
         }
     }
 }
